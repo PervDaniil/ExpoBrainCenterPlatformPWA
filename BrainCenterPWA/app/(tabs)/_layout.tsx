c4890411acc147d80f01159df7e96b9e2c536bf4
PervDaniil <studentdan123.900@gmail.com>
2025-05-23 14:39:49 +0600

    WebView implementation

--- BrainCenterPWA/app/(tabs)/_layout.tsx
+++ BrainCenterPWA/app/(tabs)/_layout.tsx
@@ -1,45 +1,24 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { WebView } from 'react-native-webview';
+import { StyleSheet, View } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+export default function HomeScreenLayout() {
+    return (
+        <>
+            <StatusBar hidden={true} />
+            <View style={styles.container}>
+                <WebView
+                    source={{ uri: 'https://pervdaniil.github.io/BrainCenterPlatform/' }}
+                    style={styles.webview}
+                />
+            </View>
+        </>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: { flex: 1 },
+    webview: { flex: 1 },
+});
